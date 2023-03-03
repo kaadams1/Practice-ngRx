23f3d509f491582c08740a040249374f82ad4bc5
kaadams1 <kaadams1@gmail.com>
2023-03-03 11:26:37 -0700

    Added NgRX entity

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -6,6 +6,9 @@ import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialo
 import { MatDialog } from '@angular/material/dialog';
 import {map, shareReplay} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../reducers';
+import { selectAdvancedCourses, selectBeginnerCourses, selectPromoTotal } from '../courses.selectors';
 
 
 
@@ -18,16 +21,13 @@ export class HomeComponent implements OnInit {
 
     promoTotal$: Observable<number>;
 
-    loading$: Observable<boolean>;
-
     beginnerCourses$: Observable<Course[]>;
 
     advancedCourses$: Observable<Course[]>;
 
 
     constructor(
-      private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
+      private dialog: MatDialog, private store: Store<AppState>) {
 
     }
 
@@ -37,29 +37,11 @@ export class HomeComponent implements OnInit {
 
   reload() {
 
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay()
-      );
+    this.beginnerCourses$ = this.store.pipe(select(selectBeginnerCourses));
 
-    this.loading$ = courses$.pipe(map(courses => !!courses));
+    this.advancedCourses$ = this.store.pipe(select(selectAdvancedCourses));
 
-    this.beginnerCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'BEGINNER'))
-      );
-
-
-    this.advancedCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'ADVANCED'))
-      );
-
-    this.promoTotal$ = courses$
-        .pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
+    this.promoTotal$ = this.store.pipe(select(selectPromoTotal));
 
   }
 
